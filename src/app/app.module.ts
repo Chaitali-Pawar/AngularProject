e1b0ed370cce4bf8808145a5079105dca1cfa082
Chaitali Pawar <chaitali2994@gmail.com>
2018-03-18 18:43:04 +0530

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,17 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
-import {DishService} from './services/dish.service'
+import {DishService} from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishDetailComponent
+    DishDetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
