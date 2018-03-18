d91e8e50abdf59652ec59820351ef847d019cd84
Chaitali Pawar <chaitali2994@gmail.com>
2018-03-18 13:12:58 +0530

    services angular

---
+++ src/app/dish-detail/dish-detail.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit,Input } from '@angular/core';
+import {Dish} from '../shared/dish';
+
+
+
+@Component({
+  selector: 'app-dish-detail',
+  templateUrl: './dish-detail.component.html',
+  styleUrls: ['./dish-detail.component.css']
+})
+export class DishDetailComponent implements OnInit {
+
+  @Input()
+  dish: Dish; 
+  
+  constructor() {
+   
+   }
+
+  ngOnInit() {
+  }
+
+}
