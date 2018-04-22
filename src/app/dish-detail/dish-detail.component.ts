e351f98274bac6cb2783f4d524b8c91bb31727b4
Chaitali Pawar <chaitali2994@gmail.com>
2018-04-22 16:43:54 +0530

    animations part 2

--- src/app/dish-detail/dish-detail.component.ts
+++ src/app/dish-detail/dish-detail.component.ts
@@ -8,23 +8,22 @@ import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
 import {Comment} from '../shared/Comment';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+//import { trigger, state, style, animate, transition } from '@angular/animations';
+
+import { visibility } from '../animations/app.animation';
+import { flyInOut  , expand } from '../animations/app.animation';
 @Component({
   selector: 'app-dish-detail',
   templateUrl: './dish-detail.component.html',
   styleUrls: ['./dish-detail.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    visibility(),
+    flyInOut(),
+    expand()
   ]
   
 })
