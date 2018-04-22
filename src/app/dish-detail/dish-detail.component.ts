bcbca4e6fda4a170964586895164a9a1abb7a79b
Chaitali Pawar <chaitali2994@gmail.com>
2018-04-22 10:07:46 +0530

    animations part 1

--- src/app/dish-detail/dish-detail.component.ts
+++ src/app/dish-detail/dish-detail.component.ts
@@ -8,11 +8,25 @@ import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
 import {Comment} from '../shared/Comment';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'app-dish-detail',
   templateUrl: './dish-detail.component.html',
-  styleUrls: ['./dish-detail.component.scss']
+  styleUrls: ['./dish-detail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
+  
 })
 export class DishDetailComponent implements OnInit {
 
@@ -25,6 +39,7 @@ export class DishDetailComponent implements OnInit {
   commentForm : FormGroup;
   comment :Comment;
   errMess: string;
+  visibility = 'shown';
 
   formErrors = {
     'comment': '',
@@ -58,8 +73,8 @@ export class DishDetailComponent implements OnInit {
     this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     // switchMap allows us to use Observable params which is set using the dish service method and then we subscribe
     // to this observable also set the prev and next , hence whenever the param changes the prev and nexr are set.
-     this.route.params.switchMap((params : Params) => this.dishService.getDish(+params['id']) )
-     .subscribe(dish => {this.dish = dish; this.dishCopy = dish; this.setPrevNext(dish.id)},
+     this.route.params.switchMap((params : Params) => {this.visibility = 'hidden'; return this.dishService.getDish(+params['id'])} )
+     .subscribe(dish => {this.dish = dish; this.dishCopy = dish; this.setPrevNext(dish.id) ,this.visibility = 'shown';},
                 errmess => this.errMess = <any>errmess); 
 
     
